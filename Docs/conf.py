e86ffa9583055a0c49aa324ba29a17aec38dc5ba
Kim Hyeong June <york0095@naver.com>
2022-12-09 18:50:57 +0900

    eraseex

--- Docs/conf.py
+++ Docs/conf.py
@@ -26,7 +26,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# extensions = ['myst_parser']
+# ##extensions = ['myst_parser']
 # source_suffix = {
 #     '.rst': 'restructuredtext',
 #     '.txt': 'markdown',
